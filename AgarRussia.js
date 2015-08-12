9ca3a4715d9c5e989a8f5f782f0eccfb16401760
DimaRRR <rozanov_dima@mail.ru>
2015-08-12 23:20:42 +0300

    Update AgarRussia.js

--- AgarRussia.js
+++ AgarRussia.js
@@ -1,46 +1,74 @@
 // ==UserScript==
-// @name         AgarRussia
+// @name         AgarRussia 1.3
 // @namespace    none
-// @version      1.2
-// @description  AgarRussia
+// @version      1.3
+// @description  AgarRussia 1.3
 // @author       DimaRRR
 // @match        http://agar.io/*
 // @grant        none
 // ==/UserScript==
 
-var version = "1.2"; // Версия клиентского расширения
+var version = "1.3"; // Версия клиентского расширения
+
+// Отправка запроса на сервер с запретом кэширования страницы
 window.jQuery.ajax({
     url: "http://pshort.ru/agarrussia",
     cache: false
 }).done(function(io) {
-    var io = io.split("_");
-	$('.agario-promo').css('background-image', 'url()');
-    $('.agario-promo').html('<div class="agario-panel agario-side-panel"><div class="clearfix" style="margin-bottom: 16px;"><center><span class="text-muted">Разработчик данного расширения: DimaRRR.<br>Установленная версия расширения: ' + version + '.</span></center></div>');
-    if (io[0] != version) {
+    var io = io.split("_"); // Разбираем ответ сервера
+	// Удаляем правый столбик и рекламу в среднем столбике
+	$('.side-container')[1].remove();
+	$('hr')[1].remove();
+	// Добавляем в левый столбик информацию о расширении
+    $('.agario-profile-panel').append('Разработчик данного расширения: DimaRRR.<br>Установленная версия расширения: ' + version + '.');
+    // Проверяем версию расширения с сервера
+	if (io[0] != version) {
+	    // Уведомляем об обновлении расширения
         $('#mainPanel').html('<h3>Новое обновление!</h3><br>Внимание!<br>Вышло новое обновление расширения AgarRussia!<br>Версия: ' + io[0] + '<br>Загрузить обновление можно перейдя по ссылке: <a href="' + io[1] + '">AgarRussia.JS</a>');
-    } else if (io[0] = version) {
+    } else if (io[0] == version) {
+	    // Проверяем параметры работы расширения
         if (io[2] == 'closed') {
-            var closed = true;
+            var closed = true; // Не работает
         } else {
-            var closed = false;
+            var closed = false; // Работает
 		}
         // Русификация и частичное изменение игровой области
         if (closed == false) {
-            $('#overlays').append('<div style="position: absolute;bottom: 0;left: 0;background-color: #FFF;border-radius: 0px 5px 0px 0px;padding: 5px 10px;"><center>AgarRussia</center></div>');
+		    // Удаление рекламных блоков
+			$('#a300x250')[0].remove();
+			//$('#adsBottom')[0].remove();
+			// Выставление названия расширения
             $('.tosBox').html('<center>AgarRussia</center>');
-            $('#adsBottom').html('<div id="adsBottomInner" style="margin: 0px auto;width: 728px;height: 21px;border: 5px solid white;border-radius: 5px 5px 0px 0px;background-color: #FFFFFF;box-sizing: content-box;"><div id="a728x90"><center>Реклама удалена с помощью расширения AgarRussia.</center></div></div>');
-            $('#options').html('<label><input type="checkbox" onchange="setSkins(!$(this).is(\':checked\'));"><span data-itr="option_no_skins">Без скинов</span></label><label><input type="checkbox" onchange="setNames(!$(this).is(\':checked\'));"><span data-itr="option_no_names">Без имен</span></label><label><input type="checkbox" onchange="setColors($(this).is(\':checked\'));"><span data-itr="option_no_colors">Без цветов</span></label><label><input type="checkbox" onchange="setShowMass($(this).is(\':checked\'));"><span data-itr="option_show_mass">Показывать очки</span></label><label><input type="checkbox" onchange="setDarkTheme($(this).is(\':checked\'));"><span data-itr="option_dark_theme">Темная тема</span></label><label><input type="checkbox" onchange="setSkipStats($(this).is(\':checked\'));"><span data-itr="option_skip_stats">Обнулить статистику</span></label>');
-            $('#a300x250').html('<center>Реклама удалена с помощью расширения AgarRussia.</center>');
+			// Русификация настроек
+            $("[data-itr=option_no_skins]").html('Без скинов');
+			$("[data-itr=option_no_names]").html('Без имен');
+			$("[data-itr=option_no_colors]").html('Без цветов');
+			$("[data-itr=option_show_mass]").html('Показывать массу');
+			$("[data-itr=option_dark_theme]").html('Тёмная тема');
+			$("[data-itr=option_skip_stats]").html('Обнулить статистику');
+			// Русификация кнопок
             $('.btn-needs-server').html('Наблюдать');
             $('.btn-logout').html('Выйти');
             $('[data-itr=continue]').html('Продолжить');
+			// Русификация поля и ввод AgarRussia
             var nick = document.getElementById("nick");
             nick.placeholder = "Логин";
             $('#nick').val('AgarRussia');
-            $('#gamemode').html('<option selected="" value="" data-itr="gamemode_ffa">Сам за себя</option><option value=":teams" data-itr="gamemode_teams">Командный</option><option value=":experimental" data-itr="gamemode_experimental">Экспериментальный</option><option value=":party" data-itr="party">Вечеринка</option>');
-            $('#instructions').html('<hr><center><span class="text-muted"><span data-itr="instructions_mouse">Двигайте мышью для управления шариком</span><br><span data-itr="instructions_space">Нажмите <b>ПРОБЕЛ</b> для разделения шарика</span><br><span data-itr="instructions_w">Нажмите <b>W</b> для извлечения некоторой массы</span><br></span></center>');
-      	    $('#agario-main-buttons').html('<button type="button" onclick="$(\'#settings, #instructions\').toggle();return false;" class="btn btn-info btn-settings"><i class="glyphicon glyphicon-cog"></i></button><button type="submit" onclick="setNick(document.getElementById(\'nick\').value); return false;" class="btn btn-play btn-primary btn-needs-server" data-itr="play">Играть</button><button type="submit" onclick="setNick(document.getElementById(\'nick\').value); return false;" class="btn btn-play-guest btn-success btn-needs-server" data-itr="play_as_guest">Играть как гость</button><button onclick="facebookLogin(); return false;" class="btn btn-login btn-primary" style="line-height: 24px;" data-original-title="" title=""><span class="social social-facebook" style="font-size:24px;margin-top:-6px;margin-left:-8px;vertical-align:middle;margin-right:5px;"></span><span data-itr="login_and_play">Войти и играть</span></button><br clear="both">');
-            $("[data-itr=stats_food_eaten]").html('Съедено семок');
+			// Русификация режимов игры
+			$("[data-itr=gamemode_ffa]").html('Сам за себя');
+			$("[data-itr=gamemode_teams]").html('Командный');
+			$("[data-itr=gamemode_experimental]").html('Экспериментальный');
+			$("[data-itr=party]").html('Вечеринка');
+            // Русификация управления игрой
+			$("[data-itr=instructions_mouse]").html('Двигайте мышью для управления шариком');
+			$("[data-itr=instructions_space]").html('Нажмите <b>ПРОБЕЛ</b> для разделения шарика');
+			$("[data-itr=instructions_w]").html('Нажмите <b>W</b> для извлечения некоторой массы');
+			// Русификация кнопок
+			$("[data-itr=play]").html('Играть');
+			$("[data-itr=play_as_guest]").html('Играть как гость');
+			$("[data-itr=login_and_play]").html('Войти и играть');
+            // Русификация окончания игры
+			$("[data-itr=stats_food_eaten]").html('Съедено семок');
             $("[data-itr=stats_highest_mass]").html('Масса');
             $("[data-itr=stats_time_alive]").html('Время игры');
             $("[data-itr=stats_leaderboard_time]").html('Время в ТОП');
@@ -50,4 +78,6 @@ window.jQuery.ajax({
             $('#a300x250').html('<center>Расширение AgarRussia готовится к обновлению. Будьте терпеливыми и ожидайте нас обновленными! Спасибо, что Вы с нами!</center>');
         }
     }
+}).fail(function() {
+    $('#mainPanel').html('<h3>Ошибка подключения</h3><br>Внимание!<br>Произошла ошибка подключения к серверам AgarRussia. Пожалуйста обновите страницу.');
 });
